4b325641a4e984c67bb90c51ae379179ef778aa1
Eagle1967 <icsourcery@gmail.com>
2012-08-16 15:14:32 -0600

    add back Notification open/close from nav bar - KhasMek

--- src/com/sourcery/magiccontrol/fragments/Navbar.java
+++ src/com/sourcery/magiccontrol/fragments/Navbar.java
@@ -652,6 +652,9 @@ public class Navbar extends SettingsPreferenceFragment implements
             } else if (uri.equals("**power**")) {
 
                 return getResources().getDrawable(R.drawable.ic_sysbar_power);
+            } else if (uri.equals("**notifications**")) {
+ 		
+                return getResources().getDrawable(R.drawable.ic_sysbar_notifications);
             } else if (uri.equals("**screenshot**")) {
  	 	
            return getResources().getDrawable(R.drawable.ic_sysbar_screenshot);
@@ -695,7 +698,9 @@ public class Navbar extends SettingsPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_kill);
             else if (uri.equals("**power**"))
                 return getResources().getString(R.string.navbar_action_power);
-             else if (uri.equals("**screenshot**"))
+            else if (uri.equals("**notifications**"))
+ 	        return getResources().getString(R.string.navbar_action_notifications);
+            else if (uri.equals("**screenshot**"))
  	         return getResources().getString(R.string.navbar_action_screenshot);
             else if (uri.equals("**null**"))
                 return getResources().getString(R.string.navbar_action_none);

ac583706d673a82416fb8cc240b1e5d291e7002a
Eagle1967 <icsourcery@gmail.com>
2012-09-05 19:56:02 -0600

    enable NavBar controls in power dialog--add power dialog --Zaphod-Beeblebrox

---
+++ src/com/sourcery/magiccontrol/fragments/PowerMenu.java
@@ -0,0 +1,85 @@
+
+package com.sourcery.magiccontrol.fragments;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+
+import com.sourcery.magiccontrol.R;
+import com.sourcery.magiccontrol.R.xml;
+
+public class PowerMenu extends PreferenceFragment {
+
+    private static final String PREF_SCREENSHOT = "show_screenshot";
+    //private static final String PREF_TORCH_TOGGLE = "show_torch_toggle";
+    private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
+    private static final String PREF_NAVBAR_HIDE = "show_navbar_hide";
+
+    CheckBoxPreference mShowScreenShot;
+    //CheckBoxPreference mShowTorchToggle;
+    CheckBoxPreference mShowAirplaneToggle;
+    CheckBoxPreference mShowNavBarHide;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.prefs_powermenu);
+
+    
+         mShowScreenShot = (CheckBoxPreference) findPreference(PREF_SCREENSHOT);
+        mShowScreenShot.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
+                0) == 1);
+
+       /* mShowTorchToggle = (CheckBoxPreference) findPreference(PREF_TORCH_TOGGLE);
+        mShowTorchToggle.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
+                0) == 1);*/
+
+        mShowAirplaneToggle = (CheckBoxPreference) findPreference(PREF_AIRPLANE_TOGGLE);
+        mShowAirplaneToggle.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
+                1) == 1);
+        
+
+        mShowNavBarHide = (CheckBoxPreference) findPreference(PREF_NAVBAR_HIDE);
+        mShowNavBarHide.setChecked(Settings.System.getBoolean(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
+                false));
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+         if (preference == mShowScreenShot) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
+                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
+            return true;
+       /* } else if (preference == mShowTorchToggle) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            return true;*/
+        } else if (preference == mShowAirplaneToggle) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
+                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
+            return true;
+        } else 
+        if (preference == mShowNavBarHide) {
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+                    Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
+                    ((CheckBoxPreference)preference).isChecked());
+            return true;
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}

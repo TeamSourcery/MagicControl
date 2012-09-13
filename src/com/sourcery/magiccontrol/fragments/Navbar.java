de15c8b65ff7fc0e6559937a60ff96005ec34dae
Patrick L <devortex@devortex>
2012-09-13 16:00:18 -0400

    update navbar color picker -more consitent with other color pickers

--- src/com/sourcery/magiccontrol/fragments/Navbar.java
+++ src/com/sourcery/magiccontrol/fragments/Navbar.java
@@ -61,7 +61,6 @@ import com.sourcery.magiccontrol.widgets.NavBarItemPreference;
 import com.sourcery.magiccontrol.widgets.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
-import com.sourcery.magiccontrol.fragments.ColorPreference;
 
 public class Navbar extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener, ShortcutPickerHelper.OnPickListener, Preference.OnPreferenceClickListener {
@@ -77,6 +76,8 @@ public class Navbar extends SettingsPreferenceFragment implements
     private static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
     private static final String NAVIGATION_BAR_HEIGHT_LANDSCAPE = "navigation_bar_height_landscape";
     private static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
+    private static final String PREF_NAV_BAR_COLOR = "interface_navbar_color";
+    private static final String PREF_NAV_BAR_COLOR_DEF = "interface_navbar_color_default";
 
     public static final int REQUEST_PICK_CUSTOM_ICON = 200;
     public static final int REQUEST_PICK_LANDSCAPE_ICON = 201;
@@ -96,7 +97,7 @@ public class Navbar extends SettingsPreferenceFragment implements
     ListPreference mNavigationBarHeightLandscape;
     ListPreference mNavigationBarWidth;
     SeekBarPreference mButtonAlpha;
-    ColorPreference mNavBar;
+    ColorPickerPreference mNavBar;
     Preference mStockColor;
 
     private int mPendingIconIndex = -1;
@@ -180,11 +181,10 @@ public class Navbar extends SettingsPreferenceFragment implements
         mNavigationBarWidth = (ListPreference) findPreference("navigation_bar_width");
         mNavigationBarWidth.setOnPreferenceChangeListener(this);
 
-        mNavBar = (ColorPreference) findPreference("interface_navbar_color");
-        mNavBar.setProviderTarget(Settings.System.SYSTEMUI_NAVBAR_COLOR,
-                                  Settings.System.SYSTEMUI_NAVBAR_COLOR_DEF);
+        mNavBar = (ColorPickerPreference) findPreference(PREF_NAV_BAR_COLOR);
+        mNavBar.setOnPreferenceChangeListener(this);
  	
-        mStockColor = (Preference) findPreference("interface_navbar_color_default");
+        mStockColor = (Preference) findPreference(PREF_NAV_BAR_COLOR_DEF);
         mStockColor.setOnPreferenceClickListener(this);
 
         if (mTablet) {
@@ -336,6 +336,14 @@ public class Navbar extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_TINT, intHex);
             return true;
+        } else if (preference == mNavBar) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SYSTEMUI_NAVBAR_COLOR, intHex);
+            return true;
         } else if (preference == mNavigationBarGlowColor) {
             String hex = ColorPickerPreference.convertToARGB(
                     Integer.valueOf(String.valueOf(newValue)));
@@ -374,7 +382,7 @@ public class Navbar extends SettingsPreferenceFragment implements
         // TODO Auto-generated method stub
         if (pref.equals(mStockColor)) {
             Settings.System.putInt(getActivity().getContentResolver(),
-                     Settings.System.SYSTEMUI_NAVBAR_COLOR, -1);
+                     Settings.System.SYSTEMUI_NAVBAR_COLOR, Settings.System.SYSTEMUI_NAVBAR_COLOR_DEF);
          }
          return false;
     }

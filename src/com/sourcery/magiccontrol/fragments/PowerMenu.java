0f658cdc4752761e949e33fa4512334594e4d642
eagle1967 <icsourcery@gmail.com>
2012-12-04 20:36:40 -0700

    re-add alpha choices to colors, default glow dots to 00000000 , revert power menu additions for now as it caused a nice system ui constant crash :)

--- src/com/sourcery/magiccontrol/fragments/PowerMenu.java
+++ src/com/sourcery/magiccontrol/fragments/PowerMenu.java
@@ -14,14 +14,16 @@ import com.sourcery.magiccontrol.R.xml;
 
 public class PowerMenu extends PreferenceFragment {
 
-    private static final String PREF_SCREENSHOT = "show_screenshot";
-    private static final String PREF_TORCH_TOGGLE = "show_torch_toggle";
-    private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
+    //private static final String PREF_POWER_SAVER = "show_power_saver";
+    //private static final String PREF_SCREENSHOT = "show_screenshot";
+    //private static final String PREF_TORCH_TOGGLE = "show_torch_toggle";
+    //private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
     private static final String PREF_NAVBAR_HIDE = "show_navbar_hide";
 
-    CheckBoxPreference mShowScreenShot;
-    CheckBoxPreference mShowTorchToggle;
-    CheckBoxPreference mShowAirplaneToggle;
+    //CheckBoxPreference mShowPowerSaver;
+    //CheckBoxPreference mShowScreenShot;
+    //CheckBoxPreference mShowTorchToggle;
+    //CheckBoxPreference mShowAirplaneToggle;
     CheckBoxPreference mShowNavBarHide;
 
     @Override
@@ -30,38 +32,54 @@ public class PowerMenu extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_powermenu);
-     
-       
+        /*
+        mShowPowerSaver = (CheckBoxPreference) findPreference(PREF_POWER_SAVER);
+        int powerSaverVal = 0;
+        try {
+            powerSaverVal = Settings.Secure.getInt(getActivity()
+                    .getContentResolver(), Settings.Secure.POWER_SAVER_MODE);
+        } catch (SettingNotFoundException e) {
+            mShowPowerSaver.setEnabled(false);
+            mShowPowerSaver
+                    .setSummary("You need to enable power saver before you can see it in the power menu.");
+        }
+        mShowPowerSaver.setChecked(powerSaverVal != 0);
+
         mShowTorchToggle = (CheckBoxPreference) findPreference(PREF_TORCH_TOGGLE);
         mShowTorchToggle.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
-                0) == 1); 
+                0) == 1);
 
         mShowScreenShot = (CheckBoxPreference) findPreference(PREF_SCREENSHOT);
         mShowScreenShot.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
                 0) == 1);
-        
+
         mShowAirplaneToggle = (CheckBoxPreference) findPreference(PREF_AIRPLANE_TOGGLE);
         mShowAirplaneToggle.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
                 1) == 1);
-       
+        */
         mShowNavBarHide = (CheckBoxPreference) findPreference(PREF_NAVBAR_HIDE);
         mShowNavBarHide.setChecked(Settings.System.getBoolean(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
-                false));  
+                false));
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-           
-       if (preference == mShowScreenShot) {
+            /*
+        if (preference == mShowScreenShot) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
                     ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
             return true;
-        
+
+        } else if (preference == mShowPowerSaver) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_DIALOG_SHOW_POWER_SAVER,
+                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
+            return true;
         } else if (preference == mShowTorchToggle) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
@@ -73,12 +91,12 @@ public class PowerMenu extends PreferenceFragment {
                     Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
                     ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
             return true;
-        } else 
+        } else */
         if (preference == mShowNavBarHide) {
             Settings.System.putBoolean(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
                     ((CheckBoxPreference)preference).isChecked());
-            return true; 
+            return true;
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);

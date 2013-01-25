977e815998bcbd633fce9a5ed77175d93def9948
eagle1967 <icsourcery@gmail.com>
2013-01-25 01:34:48 -0700

    implement windows style action apps in navbar buttons-MarcLandis

--- src/com/sourcery/magiccontrol/fragments/Navbar.java
+++ src/com/sourcery/magiccontrol/fragments/Navbar.java
@@ -724,6 +724,8 @@ public class Navbar extends SettingsPreferenceFragment implements
                 return getResources().getDrawable(R.drawable.ic_sysbar_back);
             } else if (uri.equals("**recents**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_recent);
+            } else if (uri.equals("**recentapps**")) {
+	        return getResources().getDrawable(R.drawable.ic_sysbar_recent_apps);
             } else if (uri.equals("**search**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_search);
             } else if (uri.equals("**screenshot**")) {
@@ -770,6 +772,8 @@ public class Navbar extends SettingsPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_back);
             else if (uri.equals("**recents**"))
                 return getResources().getString(R.string.navbar_action_recents);
+            else if (uri.equals("**recentapps**"))
+                return getResources().getString(R.string.navbar_action_recent_apps);
             else if (uri.equals("**search**"))
                 return getResources().getString(R.string.navbar_action_search);
             else if (uri.equals("**screenshot**"))
